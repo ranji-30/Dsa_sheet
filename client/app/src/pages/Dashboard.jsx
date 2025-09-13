c810b41b54debb6a42945323ac0d4d2e78b72df4
ranjitha-2024 <vglranjithamohan@gmail.com>
2025-09-13 19:51:12 +0530

    Dashboard fix

--- client/app/src/pages/Dashboard.jsx
+++ client/app/src/pages/Dashboard.jsx
@@ -3,14 +3,16 @@ import Navbar from "../components/Navbar";
 
 
 export default function Dashboard() {
- const user = JSON.parse(localStorage.getItem("user"));
+ const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : null;
 console.log(user)
+
   return (
     <div>
       <Navbar />
       <div className="container mt-5">
         <div className="card shadow p-4">
-           <h2 className="text-center mb-3">Hello "{user.name}"</h2>
+           <h2 className="text-center mb-3">Hello "{user ? user.name : "guest"}"</h2>
           <h2 className="text-center mb-3">Welcome to Your DSA Dashboard 🎯</h2>
           <p className="text-center text-muted">
             Track your progress, solve problems, and become a DSA master!
