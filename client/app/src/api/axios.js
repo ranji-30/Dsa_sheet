fdbcc224951e4b0af703c59aa159c5ca47790ce3
ranjitha-2024 <vglranjithamohan@gmail.com>
2025-09-13 17:50:39 +0530

    fixing the frontend link

--- client/app/src/api/axios.js
+++ client/app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // change to AWS backend later
+  baseURL: "https://dsa-sheet-8g1e.onrender.com/api", // change to AWS backend later
 });
 
 export default API;
