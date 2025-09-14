ec79f126d16cf70c72cfbff5f5d49df1ff18b2e0
ranjitha-2024 <vglranjithamohan@gmail.com>
2025-09-14 16:02:05 +0530

    axios

--- client/app/src/api/axios.js
+++ client/app/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL||'https://dsa-sheet-9q0f.onrender.com/', // picks dev or prod
+  baseURL: import.meta.env.VITE_API_BASE_URL||'https://dsa-sheet-22k2.onrender.com/api', // picks dev or prod
 });
 export default API;
